a036f212a6b5257eb10f16415ca4d438023f06b8
David <learnitmyway@users.noreply.github.com>
2020-04-05 18:01:06 +0200

    backend: Creat todo with validation

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,18 +1,51 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import {
-//   APIGatewayProxyEvent,
-//   APIGatewayProxyHandler,
-//   APIGatewayProxyResult
-// } from 'aws-lambda'
+import * as uuid from 'uuid'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult
+} from 'aws-lambda'
+import * as AWS from 'aws-sdk'
 
-// import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('createTodo')
 
-// export const handler: APIGatewayProxyHandler = async (
-//   event: APIGatewayProxyEvent
-// ): Promise<APIGatewayProxyResult> => {
-//   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+const docClient = new AWS.DynamoDB.DocumentClient()
 
-//   // TODO: Implement creating a new TODO item
-//   return undefined
-// }
+const todosTable = process.env.TODOS_TABLE
+
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  logger.info('Processing event: ', event)
+
+  const createdAt = new Date().toISOString()
+  const todoId = uuid.v4()
+  const parsedBody = JSON.parse(event.body)
+  const newTodo: CreateTodoRequest = {
+    ...parsedBody,
+    createdAt,
+    todoId,
+    done: false
+  }
+
+  await docClient
+    .put({
+      TableName: todosTable,
+      Item: newTodo
+    })
+    .promise()
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      newTodo
+    })
+  }
+}
