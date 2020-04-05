3366a954acedabf545aafce85d798b5c47ee1293
David <learnitmyway@users.noreply.github.com>
2020-04-05 17:17:43 +0200

    backend: Get todos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -5,16 +5,32 @@ import {
   APIGatewayProxyResult,
   APIGatewayProxyHandler
 } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
+
+  const result = await docClient
+    .scan({
+      TableName: todosTable
+    })
+    .promise()
+
+  const items = result.Items
+
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
-    body: ''
+    body: JSON.stringify({
+      items
+    })
   }
 }
