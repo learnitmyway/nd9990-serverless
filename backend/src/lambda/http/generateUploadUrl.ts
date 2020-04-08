228080631bcd39f10ce4effaea32c07726456ddc
David <learnitmyway@users.noreply.github.com>
2020-04-08 17:17:56 +0200

    backend: Get upload url

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,16 +1,39 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import {
-//   APIGatewayProxyEvent,
-//   APIGatewayProxyResult,
-//   APIGatewayProxyHandler
-// } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda'
+import * as AWS from 'aws-sdk'
 
-// export const handler: APIGatewayProxyHandler = async (
-//   event: APIGatewayProxyEvent
-// ): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
+const bucketName = process.env.IMAGES_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
-//   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-//   return undefined
-// }
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      uploadUrl: getUploadUrl(todoId)
+    })
+  }
+}
+
+function getUploadUrl(todoId: string) {
+  const s3 = new AWS.S3({
+    signatureVersion: 'v4'
+  })
+
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: todoId,
+    Expires: urlExpiration
+  })
+}
