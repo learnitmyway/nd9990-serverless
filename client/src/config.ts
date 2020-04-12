950e4439a0605a59c634e584a574b78517bfa9a8
David <learnitmyway@users.noreply.github.com>
2020-04-12 13:22:21 +0200

    client: Use env vars in config

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = process.env.REACT_APP_API_ID
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...', // Auth0 domain
-  clientId: '...', // Auth0 client id
+  // DONE: Create an Auth0 application and copy values from it into this map
+  domain: process.env.REACT_APP_DOMAIN, // Auth0 domain
+  clientId: process.env.REACT_APP_CLIENT_ID, // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
